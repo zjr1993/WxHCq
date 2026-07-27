225b07156530a11e0abb93610511564cf822592b
zhu jierong <45908433+zjr1993@users.noreply.github.com>
2026-07-27 09:21:11 +0800

    Refactor MFAudioPlayer for WASAPI integration
    Refactor audio playback logic and initialize WASAPI for audio rendering.

--- MFAudioPlayer.cpp
+++ MFAudioPlayer.cpp
@@ -4,15 +4,18 @@
 #include <mfidl.h>
 #include <mfreadwrite.h>
 #include <format>
-#pragma comment(lib, "mfplat.lib")
-#pragma comment(lib, "mfreadwrite.lib")
-#pragma comment(lib, "mfuuid.lib")
-#pragma comment(lib, "ole32.lib")
+#include <mmdeviceapi.h>
+#include <audioclient.h>
+#include <algorithm>
+#include "ringBuffer.hpp"
+const REFERENCE_TIME REFTIMES_PER_SEC = 10000000;
 
 // 安全释放宏
 template <class T> void SafeRelease(T** ppT) {
     if (*ppT) { (*ppT)->Release(); *ppT = NULL; }
 }
+
+#define notEnd(x) (!((x) & MF_SOURCE_READERF_ENDOFSTREAM))
 
 // 初始化 Source Reader 并强制输出 32-bit Float PCM
 HRESULT InitSourceReader(LPCWSTR filePath, IMFSourceReader** ppReader, WAVEFORMATEX** ppAudioFormat) {
@@ -66,65 +69,6 @@ done:
     return hr;
 }
 
-//HRESULT ReadAndVerifyPCM(IMFSourceReader* pReader, WAVEFORMATEX* pWaveFormat) {
-//    HRESULT hr = S_OK;
-//    IMFSample* pSample = NULL;
-//    IMFMediaBuffer* pBuffer = NULL;
-//    DWORD streamIndex = MF_SOURCE_READER_FIRST_AUDIO_STREAM;
-//    std::cout << "\n>>> Starting PCM Read Verification..." << std::endl;
-//
-//    DWORD flags = 0;
-//    LONGLONG timestamp = 0;
-//
-//    hr = pReader->ReadSample(streamIndex, 0, NULL, &flags, &timestamp, &pSample);
-//    if (FAILED(hr)) {
-//        std::cerr << "ReadSample failed." << std::endl;
-//        return hr;
-//    }
-//
-//    if (flags & MF_SOURCE_READERF_ENDOFSTREAM) {
-//        std::cout << "Reached end of stream immediately." << std::endl;
-//        return S_OK;
-//    }
-//
-//    if (pSample) {
-//        hr = pSample->ConvertToContiguousBuffer(&pBuffer);
-//        if (SUCCEEDED(hr)) {
-//            BYTE* pAudioData = NULL;
-//            DWORD cbBufferLength = 0;
-//
-//            hr = pBuffer->Lock(&pAudioData, NULL, &cbBufferLength);
-//            if (SUCCEEDED(hr)) {
-//                DWORD bytesPerFrame = pWaveFormat->nChannels * sizeof(float);
-//                DWORD numFrames = cbBufferLength / bytesPerFrame;
-//                std::cout << std::format("Successfully read {} bytes of PCM data.", cbBufferLength) << std::endl;
-//                std::cout << std::format("Total frames in this block: {} frames.\n", cbBufferLength) << std::endl;
-//
-//                float* pFloatData = reinterpret_cast<float*>(pAudioData);
-//
-//                std::cout << "First 5 audio frames (Values typically between -1.0 and 1.0):" << std::endl;
-//
-//                DWORD framesToPrint = min(numFrames, (DWORD)10);
-//
-//                for (DWORD i = 0; i < framesToPrint; ++i) {
-//                    std::cout << "Frame " << i << ": [ ";
-//                    for (WORD c = 0; c < pWaveFormat->nChannels; ++c) {
-//                        std::cout << pFloatData[i * pWaveFormat->nChannels + c] << " ";
-//                    }
-//                    std::cout << "]" << std::endl;
-//                }
-//                pBuffer->Unlock();
-//            }
-//            pBuffer->Release();
-//        }
-//        pSample->Release();
-//    }
-//    std::cout << "<<< Verification Complete.\n" << std::endl;
-//    return hr;
-//}
-
-
-// 连续读取 PCM 数据，跳过静音段，直到找到真实的波形数据
 HRESULT ReadAndVerifyPCM(IMFSourceReader* pReader, WAVEFORMATEX* pWaveFormat) {
     HRESULT hr = S_OK;
     IMFSample* pSample = NULL;
@@ -204,7 +148,259 @@ HRESULT ReadAndVerifyPCM(IMFSourceReader* pReader, WAVEFORMATEX* pWaveFormat) {
     return hr;
 }
 
+HRESULT InitWASAPI(WAVEFORMATEX* pWaveFormat, IAudioClient** ppAudioClient, IAudioRenderClient** ppRenderClient, HANDLE* pEvent) {
+    HRESULT hr = S_OK;
+    IMMDeviceEnumerator* pEnumerator = NULL;
+    IMMDevice* pDevice = NULL;
+    IAudioClient* pAudioClient = NULL;
+    IAudioRenderClient* pRenderClient = NULL;
+    HANDLE hEvent = NULL;
+    DWORD streamFlag  = AUDCLNT_STREAMFLAGS_EVENTCALLBACK | 
+                        AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM | 
+                        AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY;
 
+    std::cout << "\n>>> Starting WASAPI Initialization..." << std::endl;
+
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+    if (FAILED(hr)) goto done;
+    hr = pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+    if (FAILED(hr)) {
+        std::cerr << "Failed to get default audio endpoint." << std::endl;
+        goto done;
+    }
+
+    hr = pDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&pAudioClient);
+    if (FAILED(hr)) goto done;
+    hr = pAudioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, streamFlag, REFTIMES_PER_SEC, 0, pWaveFormat, NULL);
+
+    if (FAILED(hr)) {
+        std::cerr << "IAudioClient::Initialize failed. The audio format might not be supported in shared mode." << std::endl;
+        goto done;
+    }
+    hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (hEvent == NULL) {
+        hr = E_FAIL;
+        goto done;
+    }
+
+    hr = pAudioClient->SetEventHandle(hEvent);
+    if (FAILED(hr)) goto done;
+
+    hr = pAudioClient->GetService(__uuidof(IAudioRenderClient), (void**)&pRenderClient);
+    std::cout << "WASAPI Initialized Successfully!" << std::endl;
+
+    *ppAudioClient = pAudioClient;
+    (*ppAudioClient)->AddRef();
+
+    *ppRenderClient = pRenderClient;
+    (*ppRenderClient)->AddRef();
+
+    *pEvent = hEvent;
+
+
+done:
+    SafeRelease(&pEnumerator);
+    SafeRelease(&pDevice);
+    SafeRelease(&pAudioClient);
+    SafeRelease(&pRenderClient);
+    if (FAILED(hr) && hEvent) {
+        CloseHandle(hEvent);
+    }
+    return hr;
+}
+
+HRESULT StartPlaybackLoop(IAudioClient* pAudioClient, IAudioRenderClient* pRenderClient,
+HANDLE hAudioEvent, IMFSourceReader* pReader, WAVEFORMATEX* pWaveFormat) {
+    HRESULT hr=S_OK;
+    UINT32 bufferFrameCount = 0;
+    UINT32 numFramesPadding = 0;
+    UINT32 numFramesAvailable=0;
+    IMFSample* pLeftIMFSample = NULL;
+    UINT32 alreadyWrite = 0;
+    BYTE* pData = NULL;
+    bool bPlaying = true;
+    DWORD readerFlags = 0;
+    WORD channels = pWaveFormat->nChannels;
+    bool isEnd = false;
+    
+    // GetBufferSize函数可以获得 [声卡缓冲区帧数] bufferFrameCount 大小
+    hr = pAudioClient->GetBufferSize(&bufferFrameCount);
+
+    // 创建一个环形缓冲区，大小设定为声卡缓冲区的3倍
+    // 注意 [帧数] 乘以 [通道数量] 才是采样点数量 (一个采样点占据一个浮点数的字节数量 )
+    UINT32 ringBufferCapacity = bufferFrameCount * 10 * channels;
+    AudioRingBuffer ringBuffer {ringBufferCapacity};
+    
+    std::cout << "\n >>> Starting Audio Playback Loop with Ring Buffer."<<std::endl;
+
+    // 开启声卡
+    hr = pAudioClient->Start();
+
+    // 进入循环，不断的从MF源写入数据到环形缓冲区，然后再从环形缓冲区读到声卡缓冲区
+    // [MF]-->
+    //         -->[IMFSample<=>IMFMediaBuffer]-->
+    //                                            -->[pRawData<=>pFloatData]
+    // pFloatData---Write---->|环形缓冲区|######|******|
+
+    // 声卡不断从环形缓冲区读取数据
+    // |         bufferFrameCount          |
+    // |numFramesAvailable|numFramesPadding|
+    // |环形缓冲区|######|******|-->
+    //                             --read-->|numFramesAvailable|
+    // |########0000000000| 如果读入的数据量小于numFramesAvailable，剩余空姐需要用 0 填充
+
+    while (bPlaying) {
+        // 调用 waitForSingleObject 线程不就会暂时挂起，等待事件 hAudioEvent 触发
+        // 参数2000代表该线程最多挂起2秒
+        DWORD waitResult = WaitForSingleObject(hAudioEvent, 2000);
+        if (waitResult != WAIT_OBJECT_0) {
+            // 如果是正常情况，等待后函数WaitForSingleObject返回的是 WAIT_OBJECT_0
+            // 只要不是这种情况，立马终止循环，结束播放程序
+            std::cerr << "Audio device stalled or event timeout." << std::endl;
+            break; 
+        }
+
+
+        // ==========================================
+        // 1. 生产者逻辑：从 MF 读取数据并填入蓄水池
+        // ==========================================
+        // 只要环形缓冲区还能装下至少 12288 帧（一个安全的阈值），且文件没读完，就一直读
+        // 注释：对大多数的音频格式，虽然readSample函数返回的数据大小不一样，但是都会远小于12288帧
+        // 因此我们可以放心的认为缓冲区能装下所有readSample得到的数据
+
+        UINT32 threshold = min(4096, bufferFrameCount) * channels;
+        
+        while (ringBuffer.getFreeSpace() > threshold and (notEnd(readerFlags) or pLeftIMFSample!=NULL))
+        {
+            // 循环写入数据, 直到文件结尾或者环形缓冲区空间不够
+            IMFSample* pSample = NULL;
+
+            if (pLeftIMFSample!=NULL) {
+                // 存在遗留数据, 优先处理遗留采样数据
+                pSample = pLeftIMFSample;
+            }
+            else {
+                // 不存在遗留数据，那么就去和MF申请新采样数据
+                LONGLONG timestamp = 0;
+                // ReadSample函数给IMFSample指针赋值为指向采样数据的指针
+                hr = pReader->ReadSample(MF_SOURCE_READER_FIRST_AUDIO_STREAM,0,NULL,&readerFlags,&timestamp,&pSample);
+                if (FAILED(hr) or (readerFlags & MF_SOURCE_READERF_STREAMTICK)){
+                    std::cerr << "warning: MF_SOURCE_READERF_STREAMTICK" << std::endl;
+                    continue;
+                }
+                alreadyWrite = 0 ;
+            }
+            IMFMediaBuffer* pMediaBuffer = NULL;
+            // 将采样数据 IMFSample 映射为连续内存 IMFMediaBuffer 指针
+            if (pSample == NULL) break;
+            hr = pSample->ConvertToContiguousBuffer(&pMediaBuffer);
+            if (SUCCEEDED(hr)) {
+                BYTE* pRawData = NULL;
+                DWORD cbLength = 0;
+                // IMFMediaBuffer内存通过调用lock方法给出真正的内存指针 pRawData, cbLength代表数据大小
+                hr = pMediaBuffer->Lock(&pRawData, NULL, &cbLength);
+
+                if (SUCCEEDED(hr)) {
+                        // 数据是浮点数，因此指针转成浮点数指针
+                    float* pFloatData = reinterpret_cast<float*>(pRawData);
+
+                    // ringBuffer的方法接受的参数是采样点个数
+                    UINT32 samplesToRead = cbLength / sizeof(float);
+
+                    UINT32 actualWriteNum = ringBuffer.Write(pFloatData + alreadyWrite, samplesToRead - alreadyWrite);
+                    if (actualWriteNum < samplesToRead - alreadyWrite) {
+                        alreadyWrite += actualWriteNum;
+                        if (pLeftIMFSample == NULL) {
+                            // 之前写入没溢出，则保存这个采样pSample
+                            pLeftIMFSample = pSample;
+                            // AddRef()增加引用防止资源被释放
+                            pLeftIMFSample->AddRef();
+                        }
+                    }
+                    else {
+                        // 完美写完数据
+                        if (pLeftIMFSample != NULL){
+                            // 剩余采样pSample存在, 则先释放内存
+                            pLeftIMFSample->Release();
+                            pLeftIMFSample = NULL;
+                            alreadyWrite = 0;
+                        }
+                    }
+                    // 解除锁定
+                    pMediaBuffer->Unlock();
+                }
+                    // 释放资源
+                pMediaBuffer->Release();
+            }
+            else {
+                std::cerr << "some error happen in ConvertToContiguousBuffer" << std::endl;
+            }
+            // 释放资源
+            if (pSample != pLeftIMFSample) pSample->Release();
+        }
+
+        // ==========================================
+        // 2. 消费者逻辑：从蓄水池取水，喂给声卡
+        // ==========================================
+        // 当前已经存在的数据大小 numFramesPadding
+        hr = pAudioClient->GetCurrentPadding(&numFramesPadding);
+
+        // numFramesAvailable代表剩余需要多大数据量来填充声卡缓冲区
+        numFramesAvailable = bufferFrameCount - numFramesPadding;
+
+        if (numFramesAvailable >0 ) {
+            // pData绑定缓冲区
+            hr = pRenderClient->GetBuffer(numFramesAvailable, &pData);
+            if (SUCCEEDED(hr)) {
+                // samplesNeeded采样点数
+                UINT32 samplesNeeded = numFramesAvailable * channels;
+
+                // 转成float指针
+                float* pWasapiFloatData = reinterpret_cast<float*> (pData);
+
+                // 实际读取了多少数据
+                UINT32 samplesRead = ringBuffer.Read(pWasapiFloatData, samplesNeeded);
+
+                // std::cerr << samplesRead << std::endl;
+                if (samplesRead < samplesNeeded) {
+                    // 如果读取的数据量不够，那么必须将剩余的缓冲区填0
+                    std::cerr << "<<< Playback Stopped112." << std::endl;
+                    std::fill_n(pWasapiFloatData + samplesRead, samplesNeeded - samplesRead, 0.0f);
+                }
+
+                // 提交声卡缓存
+                hr = pRenderClient->ReleaseBuffer(numFramesAvailable, 0);
+            }
+            else {
+                std::cerr << "<<< Playback Stopped112." << std::endl;
+            }
+        }
+
+        // ==========================================
+        // 3. 退出条件：文件读完了，且环形缓冲区里的存货也播完了
+        // ==========================================
+        if ((readerFlags & MF_SOURCE_READERF_ENDOFSTREAM) and ringBuffer.getValidDataCount()==0) {
+            hr = pAudioClient->GetCurrentPadding(&numFramesPadding);
+            if (SUCCEEDED(hr) && numFramesPadding > 0) {
+                // 计算这些没播完的帧，需要多少毫秒才能播完
+                // 公式： (剩余帧数 * 1000) / 采样率
+                DWORD sleepTimeMs = (numFramesPadding * 1000) / pWaveFormat->nSamplesPerSec;
+                
+                std::cout << "Draining WASAPI hardware buffer... waiting " 
+                          << sleepTimeMs << " ms for the final notes to finish." << std::endl;
+                
+                // 多等 50 毫秒作为安全余量，确保绝对播完
+                Sleep(sleepTimeMs + 50);
+            }
+            std::cout << "End of stream and buffer flushed. Stopping playback." << std::endl;
+            bPlaying = false;
+        }
+    }
+    // 关闭声卡
+    pAudioClient->Stop();
+    std::cout << "<<< Playback Stopped." << std::endl;
+    return hr;
+}
 
 
 int main() {
@@ -212,22 +408,34 @@ int main() {
 
     // COM 和 MF 初始化
     HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
-    if (FAILED(hr)) return -1;
+    if (FAILED(hr)){
+        std::cerr << "Failed to initialize COM library." << std::endl;
+        return -1;
+    }
     hr = MFStartup(MF_VERSION);
-    if (FAILED(hr)) { CoUninitialize(); return -1; }
+    if (FAILED(hr)) {
+        std::cerr << "Failed to startup MF." << std::endl;
+        CoUninitialize();
+        return -1;
+    }
 
     std::cout << "COM and Media Foundation initialized successfully!" << std::endl;
 
     // ==========================================
     // 核心代码：加载本地音频文件
     // ==========================================
-    IMFSourceReader* pReader = NULL;
-    WAVEFORMATEX* pWaveFormat = NULL;
+    IMFSourceReader* pReader          = NULL;
+    WAVEFORMATEX* pWaveFormat         = NULL;
+    IAudioClient* pAudioClient        = NULL;
+    IAudioRenderClient* pRenderClient = NULL;
+    HANDLE hAudioEvent                = NULL;
 
     // 请替换为你电脑上一首真实存在的音频文件路径 (注意使用宽字符 L)
-    LPCWSTR audioFilePath = L"C:\\Fedora40_202606\\Eglish_study\\KamalaHarris\\KamalaHarris01.mp3";
+    LPCWSTR audioFilePath = L"C:\\Fedora40_202606\\Eglish_study\\KamalaHarris\\KamalaHarris02.mp3";
 
     std::wcout << L"Loading file: " << audioFilePath << std::endl;
+
+    // 初始化解码器
     hr = InitSourceReader(audioFilePath, &pReader, &pWaveFormat);
 
     if (SUCCEEDED(hr)) {
@@ -238,20 +446,30 @@ int main() {
         std::cout << "Format Tag: " << pWaveFormat->wFormatTag << " (3 = IEEE Float)" << std::endl;
         std::cout << "-----------------------------------------\n" << std::endl;
 
-        ReadAndVerifyPCM(pReader, pWaveFormat);
+        // 初始化声卡
+        hr = InitWASAPI(pWaveFormat, &pAudioClient, &pRenderClient, &hAudioEvent);
+
+        if (SUCCEEDED(hr)) {
+            std::cout << "-> WASAPI initialized and connected to default audio endpoint." << std::endl;
+            StartPlaybackLoop(pAudioClient,pRenderClient, hAudioEvent, pReader, pWaveFormat);
+            CloseHandle(hAudioEvent);
+            SafeRelease(&pRenderClient);
+            SafeRelease(&pAudioClient);
+        }
+        else {
+            std::cerr << "Failed to initialize WASAPI." << std::endl;
+        }
+        // ==========================================
+        // 清理资源
+        // ==========================================
+        if (pWaveFormat) {
+            CoTaskMemFree(pWaveFormat); // WAVEFORMATEX 是用 CoTaskMemAlloc 分配的，必须用此函数释放
+        }
+        if (pReader) SafeRelease(&pReader);
     }
     else {
         std::cerr << "Failed to initialize Source Reader. Error: " << std::hex << hr << std::endl;
     }
-
-    // ==========================================
-    // 清理资源
-    // ==========================================
-    if (pWaveFormat) {
-        CoTaskMemFree(pWaveFormat); // WAVEFORMATEX 是用 CoTaskMemAlloc 分配的，必须用此函数释放
-    }
-    SafeRelease(&pReader);
-
     MFShutdown();
     CoUninitialize();
 
